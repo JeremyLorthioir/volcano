5e164adc79c3d86764c98e0c93c791ede74b5fe4
Sebastian Kolind Sørensen <sks1993@gmail.com>
2017-08-27 14:28:50 +0200

    Make router clever

--- site/plugins/v-admin/router.php
+++ site/plugins/v-admin/router.php
@@ -3,18 +3,40 @@
  * @desc - get plugin pages according to URL
  */
 
-$path = get_plugin_path('v-admin') . '/pages';
+$tmpl_path = get_plugin_path('v-admin') . '/templates';
 
 if (isset(url_params()[1])) {
 	$page = url_params()[1];
 
 	if ($page === 'logout') {
 		va_logout();
-	} elseif (!file_exists($path . "/${page}.php")) {
-		die("Plugin page names $page does not exist.");
-	}
+	} elseif (is_dir($tmpl_path . "/$page")) {
+		$file = '';
 
-	require $path . "/${page}.php";
+		foreach (url_params() as $key => $filepath) {
+			if ($key === 0) { continue; }
+
+			if (is_file($tmpl_path . "/${filepath}.php")) {
+				require $tmpl_path . "/${filepath}.php";
+			} elseif(is_dir($tmpl_path . "/${filepath}")) {
+				$file .= $filepath . '/';
+			} else {
+				if (file_exists($tmpl_path . "/${file}/${filepath}.php")) {
+					$file .= "${filepath}.php";
+				}
+			}
+		}
+
+		if (substr($file, -1) === '/') {
+			require $tmpl_path . "/$file/index.php";
+		} elseif ($file !== '') {
+			require $tmpl_path . "/$file";
+		}
+	} elseif (!file_exists($tmpl_path . "/${page}.php")) {
+		die("Plugin page named $page doesn't exist.");
+	} else {
+		require $tmpl_path . "/${page}.php";
+	}
 } else {
-	require $path . '/home.php';
+	require $tmpl_path . '/home.php';
 }

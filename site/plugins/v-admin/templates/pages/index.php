d37c8414b11e5a2b3a1a54f6b2e2245d2bb974ae
Sebastian Kolind Sørensen <skolind@skybrud.dk>
2017-11-20 09:19:05 +0100

    Show all pages in a list

--- site/plugins/v-admin/templates/pages/index.php
+++ site/plugins/v-admin/templates/pages/index.php
@@ -1,1 +1,23 @@
-This is all pages.
+<?php
+
+// Get all pages to list here
+$pages = scandir(SITE . '/pages');
+
+// Remove . and .. present on Linux
+$pages = array_diff($pages, array('.', '..'));
+
+?>
+
+<h2>Pages</h2>
+
+<ul class="va-list">
+	<?php
+		foreach ($pages as $page):
+		$modification_time = date('d/m/Y, H:i', filemtime(SITE . '/pages/' . $page));
+	?>
+		<li>
+			<a href="/admin/pages/edit/<?php echo str_replace('.md', '', $page); ?>"><?php echo $page; ?></a>
+			<time>Modified: <?php echo $modification_time; ?></time>
+		</li>
+	<?php endforeach; ?>
+</ul>
